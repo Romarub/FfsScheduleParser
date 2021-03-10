b981bae26f9d78c8799f72a2e2630948e6b3187d
Roman Rubtsov <w-raven91@mail.ru>
2021-03-10 16:24:22 +0300

    add Autofac - DI container

--- FfsScheduleParser/Program.cs
+++ FfsScheduleParser/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Configuration;
 using Autofac;
-using FfsScheduleParser.Domain;
 using FfsScheduleParser.Infrastructure;
 using FfsScheduleParser.Services;
 
