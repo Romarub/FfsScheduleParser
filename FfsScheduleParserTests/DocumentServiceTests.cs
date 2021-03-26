82d0299bad81d42137df2a5126043f205b3018ef
Roman Rubtsov <w-raven91@mail.ru>
2021-03-26 13:08:14 +0300

    add tests for trainingLog, DocumentService, TrainingSessionsService

--- FfsScheduleParserTests/DocumentServiceTests.cs
+++ FfsScheduleParserTests/DocumentServiceTests.cs
@@ -37,15 +37,64 @@
 
              };
              var docWriterMock = new Mock<IDocumentsWriter>();
-             var statisticService = new DocumentsService(docWriterMock.Object);
+             var documentsService = new DocumentsService(docWriterMock.Object);
 
-             var statistic = statisticService.GetSessionsStatistic(new[] {session}, simulators).ToArray();
+             var statistic = documentsService.GetSessionsStatistic(new[] {session}, simulators).ToArray();
 
              Assert.AreEqual(1, statistic.Length);
-             Assert.AreEqual(expectedStatistic.Simulator, statistic[0].Simulator);
-             Assert.AreEqual(expectedStatistic.NumberOfSessions, statistic[0].NumberOfSessions);
-             Assert.AreEqual(expectedStatistic.PlannedTime, statistic[0].PlannedTime);
-             Assert.AreEqual(expectedStatistic.AchievedTime, statistic[0].AchievedTime);
+             Assert.AreEqual(expectedStatistic, statistic[0]);
+         }
+
+         [TestMethod]
+         public void GetTrainingLogs_GoodCase_ReturnsCorrectLogs()
+         {
+             var trainingDate = new DateTime(2021, 3, 1);
+             var shiftEndTime = new DateTime(2021, 3, 1, 16, 15, 0);
+             var sessionInShift = new TrainingSession
+             {
+                 Simulator = SimulatorName,
+                 StartDate = trainingDate,
+                 StartTime = new TimeSpan(8, 0, 0),
+                 EndDate = trainingDate,
+                 EndTime = new TimeSpan(16, 15, 0),
+                 Status = string.Empty
+             };
+             var sessionInNextShift = new TrainingSession
+             {
+                 Simulator = SimulatorName,
+                 StartDate = trainingDate,
+                 StartTime = new TimeSpan(12, 15, 0),
+                 EndDate = trainingDate,
+                 EndTime = new TimeSpan(16, 16, 0),
+                 Status = string.Empty
+             };
+             var expectedLogs = new[]
+             {
+                 new TrainingLog
+                 {
+                     TrainingDate = sessionInShift.StartDateTime,
+                     StartTime = sessionInShift.StartTime,
+                     TrainingTime = sessionInShift.EndDateTime.Subtract(sessionInShift.StartDateTime),
+                     Simulator = sessionInShift.Simulator,
+                     IsEndsInNextShift = sessionInShift.EndDateTime > shiftEndTime
+                 },
+                 new TrainingLog
+                 {
+                     TrainingDate = sessionInNextShift.StartDateTime,
+                     StartTime = sessionInNextShift.StartTime,
+                     TrainingTime = sessionInNextShift.EndDateTime.Subtract(sessionInNextShift.StartDateTime),
+                     Simulator = sessionInNextShift.Simulator,
+                     IsEndsInNextShift = sessionInNextShift.EndDateTime > shiftEndTime
+                 }
+             };
+             var docWriterMock = new Mock<IDocumentsWriter>();
+             var documentsService = new DocumentsService(docWriterMock.Object);
+
+             var logs = documentsService.GetTrainingLogs(new[] {sessionInShift, sessionInNextShift}, shiftEndTime).ToArray();
+
+             Assert.AreEqual(2, logs.Length);
+             Assert.AreEqual(expectedLogs[0], logs[0]);
+             Assert.AreEqual(expectedLogs[1], logs[1]);
          }
      }
  }
--- FfsScheduleParserTests/TrainingSessionsServiceTests.cs
+++ FfsScheduleParserTests/TrainingSessionsServiceTests.cs
@@ -12,8 +12,8 @@ namespace FfsScheduleParserTests
     [TestClass]
     public class TrainingSessionsServiceTests
     {
-        private readonly string SimulatorName = "TestSimulator";
-        private readonly string RequestAddress = "TestAddress";
+        private const string SimulatorName = "TestSimulator";
+        private const string RequestAddress = "TestAddress";
 
         [TestMethod]
         public void GetCurrentShiftTrainingSessions_GoodCase_ReturnsTrainingSessions()
@@ -58,6 +58,57 @@ namespace FfsScheduleParserTests
                 RequestAddress,
                 trainingDate.Add(startOfInterval),
                 trainingDate.Add(startOfInterval + intervalLength))
+                .ToArray();
+
+
+            Assert.AreEqual(1, sessions.Length);
+            Assert.AreEqual(expectedSession.Simulator, sessions[0].Simulator);
+            Assert.AreEqual(expectedSession.EndTime, sessions[0].EndTime);
+        }
+
+        [TestMethod]
+        public void GetSessionsForLogs_GoodCase_ReturnsTrainingSessions()
+        {
+            var trainingDate = new DateTime(2021, 3, 1);
+            var startOfInterval = new TimeSpan(11, 0, 0);
+            var intervalLength = new TimeSpan(1, 30, 0);
+
+            var sessionInInterval = new TrainingSession
+            {
+                Simulator = SimulatorName,
+                StartDate = trainingDate,
+                StartTime = startOfInterval.Add(intervalLength),
+                EndDate = trainingDate,
+                EndTime = new TimeSpan(12, 0, 0),
+                Status = string.Empty
+            };
+            var sessionOutOfInterval = new TrainingSession
+            {
+                Simulator = SimulatorName,
+                StartDate = trainingDate,
+                StartTime = startOfInterval,
+                EndDate = trainingDate,
+                EndTime = new TimeSpan(14, 0, 0),
+                Status = string.Empty
+            };
+
+            var serializedSessions = JsonConvert.SerializeObject(new[] {sessionInInterval, sessionOutOfInterval});
+
+            var expectedSession = new TrainingSession
+            {
+                Simulator = SimulatorName,
+                EndTime = new TimeSpan(12, 0, 0)
+            };
+
+            var webClientMock = new Mock<IWebClientWrapper>();
+            webClientMock.Setup(c => c.DownloadString(RequestAddress)).Returns(serializedSessions);
+            var sessionsService = new TrainingSessionsService(webClientMock.Object);
+
+
+            var sessions = sessionsService.GetSessionsForLogs(
+                    RequestAddress,
+                    trainingDate.Add(startOfInterval),
+                    trainingDate.Add(startOfInterval + intervalLength))
                 .ToArray();
 
 
